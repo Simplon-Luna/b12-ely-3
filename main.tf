d7cc4d94ab8a5aa8d11d9713bc2e768536ebc131
Elywen <simplon.luna@gmail.com>
2023-10-05 13:39:35 +0200

    ssh

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "azurerm_linux_virtual_machine" "red-vm" {
   admin_username      = "adminuser"
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/ssh.pub")
+    public_key = file("~/git/b13-ely/.ssh/ssh.pub")
   }
 
   os_disk {
